2c66d857ae9d7d49ca618efdcad01971c63c7aed
Haidil-SMU <haidil.ms.2025@computing.smu.edu.sg>
2026-09-07 09:17:37 +0800

    exercises

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -3,16 +3,23 @@
 
     // Add code here
     // challenge: use array  
+    const message = ref("Choose your favourite")
+    const fruits = ref(["apple", "orange","peach"]);
+    // const fruits1 = ref("apple");
+    // const fruits2 = ref("orange");
+    // const fruits3 = ref("peach");
     
 </script>
 
 <template>
     {{ message }} 
    
-    <label for=""> <input type="radio" name="fruit"> {{ fruit1 }} </label> 
-    <label for=""> <input type="radio" name="fruit"> {{ fruit2 }} </label> 
-    <label for=""><input type="radio" name="fruit"> {{ fruit3 }} </label> 
-    
+    <label for=""> <input type="radio" name="fruit"> {{ fruits[0] }} </label> 
+    <label for=""> <input type="radio" name="fruit"> {{ fruits[1] }} </label> 
+    <label for=""><input type="radio" name="fruit"> {{ fruits[2] }} </label> 
+    <!-- <label v-for="fruit in fruits" :key="fruit">
+        <input type="radio" name="fruit">{[fruit]}
+    </label>  -->
 </template>
 
 <style scoped>
--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -2,11 +2,25 @@
     import { ref } from 'vue'
 
     // Add code here
+    const image  = {
+        src: '/assets/me.png',
+        width: '200',
+        height: '200px'
+    }
+
+    const obj = {
+        width: '200',
+        height: '200',
+        src: '/assets/me.png'
+    }
 
 </script>
 
 <template>
 
     <!-- Add code here -->
+     <img v-bind:src="myImg" width="width" height="height" />
+
+     <img v-bind="obj"/>
    
 </template>
